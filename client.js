9e6588b560e285b4f608390237cffeb5c3d96872
juice <juice@horseye.(none)>
2016-01-23 15:59:01 +0200

    Lots of refactoring

--- client.js
+++ client.js
@@ -7,83 +7,106 @@
 </style>
 </head>
 <body>
+<form> status: <input type="text" id="myStatusField" value="" disabled></form>
+<br>
+<table id= "myCustomerTable"> </table>
+<br>
+<table id = "myInvoiceTable"> </table>
+<br>
+<button onclick="sendAllInvoices()">Send all invoices</button>
+
 <script language="javascript" type="text/javascript">
 
-  var sendable = {type:1, content:""};
   var site = window.location.hostname;
   var mySocket = new WebSocket("ws://" + site + ":8081/");
-  var firstAccess = 0;
+  var customerArray = [];
+  var invoiceArray = [];
+
   mySocket.onopen = function (event) {
-    mySocket.send("Here is some text that the server is urgently awaiting!");
-    document.getElementById("myTextField").value = "start";
+    var sendable = {type:"clientStarted"};
+    mySocket.send(JSON.stringify(sendable));
+    document.getElementById("myStatusField").value = "started";
   };
 
   mySocket.onmessage = function (event) {
     var receivable = JSON.parse(event.data);
-    if(receivable.type == "1") {
-      document.getElementById("myTextField").value = receivable.content;
-    } else if(receivable.type == "invoiceData") {
-	console.log(receivable.type);
-	console.log(receivable.content);
-	document.body.appendChild(createTable1(receivable.content.customers,
-					       receivable.content.invoices.length));
+    if(receivable.type == "statusData") {
+      document.getElementById("myStatusField").value = receivable.content;
+    }
+    if(receivable.type == "invoiceData") {
+//      console.log(receivable.type);
+//      console.log(receivable.content);
+      customerArray = receivable.content.customers;
+      invoiceArray = receivable.content.invoices;
+
+      document.body.replaceChild(createCustomerTable(),
+				 document.getElementById("myCustomerTable"));
+
+      document.body.replaceChild(createInvoiceTable(),
+				 document.getElementById("myInvoiceTable"));
 
 
-//      if(firstAccess === 0) {
-//        firstAccess = 1;
-//        document.body.appendChild(createTable1(receivable.content));
-//      } else {
-//        document.body.replaceChild(createTable1(receivable.content), document.getElementById("myCustomerTable"));
-//      }
     } else if(receivable.type == "3") {
 
 	console.log(receivable.type);
 	console.log(receivable.content);
-	document.body.appendChild(createTable2(receivable.content));
+	document.body.appendChild(createInvoiceTable(receivable.content));
 
     }
   }
 
-</script>
-
-<form>
-  time: <input type="text" id="myTextField" value="" disabled>
-</form>
-
-<button onclick="getPreviewPdf()">Click me</button>
-
-<script language="javascript" type="text/javascript">
-
-function createTable1(tableData, invoiceRows) {
+function createCustomerTable() {
     var clientCount = 0
     var table = document.createElement('table');
+    var tableHeader = document.createElement('thead');
     var tableBody = document.createElement('tbody');
     table.id = "myCustomerTable";
 
-    tableData.forEach(function(name) {
+    var hRow0 = tableHeader.insertRow(0);    
+    var hRow1 = tableHeader.insertRow(1);    
+    var hCell0 = hRow0.insertCell(0);
+    var hCell1 = hRow0.insertCell(1);
+    var hCell2 = hRow1.insertCell(0);
+    hCell0.colSpan = "2";
+    hCell0.rowSpan = "2";
+    hCell1.colSpan = invoiceArray.length;
+    hCell0.innerHTML = "<b>Customer</b>";
+    hCell1.innerHTML = "<b>Invoices</b>";
+
+    var i = 0;
+    while(i < (invoiceArray.length)) {
+	var hCellN = hRow1.insertCell(i);
+	hCellN.innerHTML = "<b>" + (i+1) + "</b>";
+	i++;
+    }
+	
+    customerArray.forEach(function(s) {
 	clientCount = clientCount + 1;
 	var row = document.createElement('tr');
+	var cell0 = document.createElement('td');
+	cell0.appendChild(document.createTextNode(s.name));
 	var cell1 = document.createElement('td');
-	cell1.appendChild(document.createTextNode(name));
+	cell1.appendChild(document.createTextNode(s.team));
+	row.appendChild(cell0);
 	row.appendChild(cell1);
 
 	var i = 0;
-	while ( i < invoiceRows) {
+	while(i < invoiceArray.length) {
 	    var cellN = document.createElement('td');
 	    var checkbox = document.createElement('input');
 	    checkbox.type = "checkbox";
-	    checkbox.name = "cb_"+name;
+	    checkbox.name = "cb_" + clientCount + "_" + i;
 	    checkbox.value = "0"
 	    cellN.appendChild(checkbox);
 	    row.appendChild(cellN);
-	    i = i + 1;
+	    i++;
 	}
 
 	var cellP = document.createElement('td');
 	var previewLink = document.createElement('a');
 	var previewText = document.createTextNode("preview PDF");
 	previewLink.appendChild(previewText);
-	previewLink.id = "pdfPreview" + clientCount;
+	previewLink.id = clientCount;
 	previewLink.onclick = function() { getPreviewPdf(previewLink.id); }
 	previewLink.title = "preview PDF";
 	previewLink.href = "#";
@@ -93,6 +116,7 @@ function createTable1(tableData, invoiceRows) {
 	tableBody.appendChild(row);
     });
 
+    table.appendChild(tableHeader);
     table.appendChild(tableBody);
 
     return table;
@@ -100,36 +124,57 @@ function createTable1(tableData, invoiceRows) {
 
 function getPreviewPdf(s) {
     console.log("clicked preview : " + s);
-    var sendable = {type:"getPdfPreview", content:s};
+
+    var i = 0;
+    while (i < invoiceArray.length) {
+	console.log("baa");
+	i++;
+    }
+
+// "cb_" + clientCount + "_" + i;
+
+    var sendable = {type:"getPdfPreview", client:s};
     mySocket.send(JSON.stringify(sendable));
     return false;
 }
 
+function sendAllInvoices() {
+    alert("WTF!");
+}
 
-function createTable2(tableData) {
+function createInvoiceTable() {
     var table = document.createElement('table');
+    var tableHeader = document.createElement('thead');
     var tableBody = document.createElement('tbody');
     table.id = "myInvoiceTable";
 
-    tableData.rivit.forEach(function(name) {
+    var hRow = tableHeader.insertRow(0);    
+    var hCell = hRow.insertCell(0);
+    hCell.innerHTML = "<b>Invoices</b>";
+
+    var count = 1;
+    invoiceArray.forEach(function(name) {
 	console.log(name);
 	var row = document.createElement('tr');
 	var cell1 = document.createElement('td');
-	cell1.appendChild(document.createTextNode(name.description));
-	row.appendChild(cell1);
+	cell1.appendChild(document.createTextNode(count));
 	var cell2 = document.createElement('td');
-	cell2.appendChild(document.createTextNode(name.n));
-	row.appendChild(cell2);
+	cell2.appendChild(document.createTextNode(name.description));
+	row.appendChild(cell1);
 	var cell3 = document.createElement('td');
-	cell3.appendChild(document.createTextNode(name.price));
-	row.appendChild(cell3);
+	cell3.appendChild(document.createTextNode(name.n));
+	row.appendChild(cell2);
 	var cell4 = document.createElement('td');
-	cell4.appendChild(document.createTextNode(name.vat));
+	cell4.appendChild(document.createTextNode(name.price));
+	row.appendChild(cell3);
+	var cell5 = document.createElement('td');
+	cell5.appendChild(document.createTextNode(name.vat));
 	row.appendChild(cell4);
-
 	tableBody.appendChild(row);
+	count++;
     });
 
+    table.appendChild(tableHeader);
     table.appendChild(tableBody);
 
     return table;
--- pantterilasku.js
+++ pantterilasku.js
@@ -2,26 +2,34 @@ var websocket = require("websocket");
 var http = require("http");
 var fs = require("fs");
 
-try {
-    var customerData = JSON.parse(fs.readFileSync("customers.json"));
-    var invoiceData = JSON.parse(fs.readFileSync("invoices.json"));
-    var companyData = JSON.parse(fs.readFileSync("company.json"));
-} catch (err) {
-    console.log(err.message);
-    process.exit(1);
+function getFileData() {
+    try {
+	var customerData = JSON.parse(fs.readFileSync("customers.json"));
+	var invoiceData = JSON.parse(fs.readFileSync("invoices.json"));
+	var companyData = JSON.parse(fs.readFileSync("company.json"));
+    } catch (err) {
+	console.log(err.message);
+	process.exit(1);
+    }
+
+    return { customers : customerData.customers.map(function(s) { return ({ name: s.name, team: s.team }) ; }),
+	     invoices  : invoiceData.rivit,
+	     company   : companyData.company };
+}
+
+function servicelog(s) {
+    console.log((new Date()) + " --- " + s);
 }
 
 var pdfprinter = require("./pdfprinter");
 var filename = "panthers.pdf";
 var globalConnectionList = [];
-var clientSendable = { customers : customerData.customers.map(function(s) { return s.name; }),
-		       invoices  : invoiceData.rivit,
-		       company   : companyData.company };
 
-function servePage(content) {
+function serveClientPage() {
     http.createServer(function(request,response){
+	clientscript = fs.readFileSync("./client.js", "utf8");
 	response.writeHeader(200, {"Content-Type": "text/html"});
-	response.write(content);
+	response.write(clientscript);
 	response.end();
     }).listen(8080);
 }
@@ -38,31 +46,40 @@ wsServer = new websocket.server({
 });
 
 wsServer.on('request', function(request) {
-    console.log((new Date()) + " Connection from origin " + request.origin);
+    servicelog("Connection from origin " + request.origin);
     var connection = request.accept(null, request.origin);
     var index = globalConnectionList.push(connection) - 1;
-    console.log((new Date()) + " Client #" + index + " accepted");
+    var sendable;
+    servicelog("Client #" + index + " accepted");
 
     connection.on('message', function(message) {
         if (message.type === 'utf8') {
-	    console.log(JSON.parse(message));
-            var receivable = JSON.parse(message.data);
-            if(receivable.type == "getPdfPreview") {
-               console.log(receivable.content);
-            } else {
-	       var sendable = {type:"invoiceData", content:clientSendable};
-               connection.send(JSON.stringify(sendable));
+            var receivable = JSON.parse(message.utf8Data);
+
+            if(receivable.type == "clientStarted") {
+		var clientSendable = getFileData();
+		servicelog("Sending initial data to client #" + index);
+		sendable = {type:"statusData", content: "hippaheikin laiva on bullaa ja woita!"};
+		connection.send(JSON.stringify(sendable));
+		sendable = {type:"invoiceData", content:clientSendable};
+		connection.send(JSON.stringify(sendable));
+            }
+	    if (receivable.type == "getPdfPreview") {
+		servicelog("Client #" + index + " requestes PDF preview " + receivable.client);
+		var sendable = {type:"statusData", content: "Prnting preview"};
+		connection.send(JSON.stringify(sendable));
+
             }
         }
     });
 
     connection.on('close', function(connection) {
-        console.log((new Date()) + " client #" + index + " disconnected");
+        servicelog("client #" + index + " disconnected");
         globalConnectionList.splice(index, 1);
     });
 
 });
 
-servePage(fs.readFileSync("./client.js", "utf8"));
+serveClientPage();
 
 //pdfprinter.printSheet(filename, lasku, kamat);
